ab76f00868f12f55d22d6a1dde46348b91eaa019
2695471708@qq.com <2695471708@qq.com>
2020-12-28 20:17:21 +0800

    Initial Commit

---
+++ 10212.java
@@ -0,0 +1,11 @@
+public class 10212{
+	private final static String birthday="2020-12-22 21:53:00";
+	
+	public static void main(String[] args){
+	System.out.println("今天天气很好");
+	while(true){
+	String format1="yyyy-MM-dd";
+	String format2="yyyy-MM-dd HH:mm:ss";
+}
+}
+}
